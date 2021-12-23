dd56cf0217ab4845f55a0d84c41f725a887ac47b
bkrtraore <b.traore771@gmail.com>
2021-12-23 23:36:34 +0100

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Salut mec !</h1>
+        </div>
+    );
+};
+
+
+//Utilisable partout dans le projet
+export default Home;
+
